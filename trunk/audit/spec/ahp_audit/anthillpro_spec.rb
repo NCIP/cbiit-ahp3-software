d4a5e740b7e8de65e0ee42885d0a6cdc7ea0db4a
mcmahonj <mcmahonj@mail.nih.gov>
2012-08-29 08:22:21 +0000

    explicitly made the specifications pending

--- trunk/audit/spec/ahp_audit/anthillpro_spec.rb
+++ trunk/audit/spec/ahp_audit/anthillpro_spec.rb
@@ -3,31 +3,52 @@ require 'ahp_audit/anthillpro'
 module AhpAudit
 	describe AnthillPro do
 
-		it "should not allow LDAP user to login without a password"
+		it "should not allow LDAP user to login without a password" do
+      pending
+    end
 
 		it "should not allow admin to login without a password" do
-			@anthill = AhpAudit::AnthillPro.new
-				sleep 5
-			@anthill.close
+      pending
+#			@anthill = AhpAudit::AnthillPro.new
+#				sleep 5
+#			@anthill.close
 		end
 
-		it "should allow admin to login with correct password"
+		it "should allow admin to login with correct password" do
+      pending
+    end
 
-		it "should have more than 3 agents listening"
+		it "should have more than 3 agents listening" do
+      pending
+    end
 
-		it "should have more than 3 agents in the build farm"
+		it "should have more than 3 agents in the build farm" do
+      pending
+    end
 
-		it "should have Dashboard with automated and manual folders"
+		it "should have Dashboard with automated and manual folders" do
+      pending
+    end
 
-		it "should include LDAP user elkins"
+		it "should include LDAP user elkins" do
+      pending
+    end
 
-		it "should include LDAP user adamsa"
+		it "should include LDAP user adamsa" do
+      pending
+    end
 
-		it "should include LDAP user vitroa"
+		it "should include LDAP user vitroa" do
+      pending
+    end
 
-		it "should build maven archetype successfully"
+		it "should build maven archetype successfully" do
+      pending
+    end
 
-		it "should not show passwords in build logs"
+		it "should not show passwords in build logs" do
+      pending
+    end
 
 	end
 end
