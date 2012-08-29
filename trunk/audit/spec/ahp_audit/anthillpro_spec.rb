d89159ab1c4805a7f30bbc615abbd31e385b2d6d
mcmahonj <mcmahonj@mail.nih.gov>
2012-08-29 02:17:33 +0000

    add more ahp tests

--- trunk/audit/spec/ahp_audit/anthillpro_spec.rb
+++ trunk/audit/spec/ahp_audit/anthillpro_spec.rb
@@ -3,7 +3,8 @@ require 'ahp_audit/anthillpro'
 module AhpAudit
 	describe AnthillPro do
 
-	
+		it "should not allow LDAP user to login without a password" do
+		end
 
 		it "should not allow admin to login without a password" do
 			@anthill = AhpAudit::AnthillPro.new
@@ -11,18 +12,29 @@ module AhpAudit
 			@anthill.close
 		end
 
-		it "should not allow LDAP user to login without a password" do
-
+		it "should allow admin to login with correct password" do
 		end
 
-		it "should allow admin to login with a proper password" do
-
+		it "should have more than 3 agents listening" do
 		end
 
-		it "should have agents listening" do
+		it "should have more than 3 agents in the build farm" do
 		end
 
+		it "should have Dashboard with automated and manual folders" do
+		end
 
+		it "should build maven archetype successfully" do
+		end
+
+		it "should include LDAP user elkins" do
+		end
+
+		it "should include LDAP user adamsa" do
+		end
+
+		it "should include LDAP user vitroa" do
+		end
 
 	end
 end
